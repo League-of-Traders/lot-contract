6122cd26871c1c0ae9292ac3cc253f4f5704ac45
jaden <dsjoh111@gmail.com>
2025-06-02 13:57:16 +0900

    change token name

--- projects/token-farm/contracts/LotToken.sol
+++ projects/token-farm/contracts/LotToken.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.26;
 import "bsc-library/contracts/BEP20.sol";
 
 // CakeToken with Governance.
-contract LotToken is BEP20("lot test token", "ltt") {
+contract LotToken is BEP20("League of traders", "LOT") {
     /// @dev Creates `_amount` token to `_to`. Must only be called by the owner.
     function mintTo(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
